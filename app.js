d8f93c89b8dc41630a813b4720ee9b2ceb8e4267
Aswadi <aswadi1986@gmail.com>
2017-08-16 16:13:57 +0800

    start

---
+++ app.js
@@ -0,0 +1,42 @@
+var telegramBot = require('node-telegram-bot-api'); 
+ 
+var token ='387164811:AAFG-YE0wZ9rJaCt3MpGIjWZAEssYWu-LCU'; 
+ 
+var api = new telegramBot(token, {polling: true}); 
+ 
+api.onText(/\/help/, function(msg, match) { 
+  var fromId = msg.from.id; 
+  api.sendMessage(fromId, "I can help you in getting the sentiments of any text you send to me."); 
+}); 
+ 
+api.onText(/\/start/, function(msg, match) { 
+  var fromId = msg.from.id; 
+  api.sendMessage(fromId, "They call me MadansFirstTelegramBot. " +  
+    "I can help you in getting the sentiments of any text you send to me."+ 
+    "To help you i just have few commands.\n/help\n/start\n/sentiments"); 
+}); 
+ 
+var opts = { 
+  reply_markup: JSON.stringify( 
+    { 
+      force_reply: true 
+    } 
+  )}; 
+ 
+//sentiment command execution is added here 
+api.onText(/\/sentiments/, function(msg, match) { 
+  var fromId = msg.from.id;   
+  api.sendMessage(fromId, "Alright! So you need sentiments of a text from me. "+ 
+    "I can help you in that. Just send me the text.", opts) 
+  .then(function (sended) { 
+    var chatId = sended.chat.id; 
+    var messageId = sended.message_id; 
+    api.onReplyToMessage(chatId, messageId, function (message) { 
+      //call a function to get sentiments here... 
+      var sentival = sentiment(message.text); 
+      api.sendMessage(fromId,"So sentiments for your text are, Score:" + sentival.score +" Comparative:"+sentival.comparative); 
+    }); 
+  });                                                    
+}); 
+ 
+console.log("MadansFirstTelegramBot has started. Start conversations in your Telegram.");
